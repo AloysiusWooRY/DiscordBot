82a2f1c2a5fc77544f5af54f3c0f745510aa020a
Alloy22 <aloysius22@hotmail.sg>
2020-02-28 10:31:37 +0800

    Update max.js

--- cmdsForRoom/max.js
+++ cmdsForRoom/max.js
@@ -1,5 +1,26 @@
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("hi");
+
+const rp = require('request-promise');
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': process.env.crypt
+  },
+  json: true,
+  gzip: true
+};
+
+rp(requestOptions).then(response => {
+  message.channel.send('API call response:', response);
+});
+
+    //message.channel.send("hi");
 }
  
 module.exports.help = {
